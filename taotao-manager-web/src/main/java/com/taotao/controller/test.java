a0e4833428d812a49faad43fd2c82c3eacdc0a7e
tuyongjian <1475591137@qq.com>
2018-04-17 21:18:31 +0800

    提交taotao测试

---
+++ taotao-manager-web/src/main/java/com/taotao/controller/test.java
@@ -0,0 +1,11 @@
+package com.taotao.controller;
+
+/**
+ * Created by Administrator on 2018/4/17.
+ */
+public class test {
+
+    public  static void main(String [] args ){
+        System.out.println("你好啊");
+    }
+}
