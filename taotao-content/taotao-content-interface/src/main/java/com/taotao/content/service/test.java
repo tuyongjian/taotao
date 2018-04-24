e96ed79ba50ab16fb2da57e996acda5adfb82c2a
tuyongjian <1475591137@qq.com>
2018-04-24 16:25:49 +0800

    taotao练习

---
+++ taotao-content/taotao-content-interface/src/main/java/com/taotao/content/service/test.java
@@ -0,0 +1,7 @@
+package com.taotao.content.service;
+
+/**
+ * Created by len on 2018/4/24.
+ */
+public class test {
+}
---
+++ taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/test.java
@@ -0,0 +1,7 @@
+package com.taotao.content.service.impl;
+
+/**
+ * Created by len on 2018/4/24.
+ */
+public class test {
+}
